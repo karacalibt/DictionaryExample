c4629227409ad8a4e1625406cc516a0d0ac4e103
berat tanrıverdi <karacalit4@gmail.com>
2023-08-05 11:46:19 +0300

    Add project files.

---
+++ MyDictionaryExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    public class MyDictionaryExample<TKey,  TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionaryExample()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    throw new ArgumentException("Aynı anahtar zaten mevcut.");
+                }
+            }
+
+            TKey[] tempKeys = new TKey[keys.Length + 1];
+            TValue[] tempValues = new TValue[values.Length + 1];
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                tempKeys[i] = keys[i];
+                tempValues[i] = values[i];
+            }
+
+            tempKeys[keys.Length] = key;
+            tempValues[values.Length] = value;
+
+            keys = tempKeys;
+            values = tempValues;
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using DictionaryExample;
+
+Console.WriteLine("Hello, dictionary!");
+
+MyDictionaryExample<int, string> dict = new MyDictionaryExample<int, string>();
+dict.Add(25, "Player1");
+dict.Add(28, "Player2");
+dict.Add(32, "Player3");
+
+Console.WriteLine(dict.Count);
